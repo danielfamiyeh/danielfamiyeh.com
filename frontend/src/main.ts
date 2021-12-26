70badda680013a05eafde02e1599b13d5330adcd
Daniel Famiyeh <daniel.kwaku.famiyeh@gmail.com>
2021-12-26 18:57:01 +0000

    Set up apollo client

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,12 @@ import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { apolloProvider } from './services/apollo';
 
 const app = createApp({
   render: () => h(App)
 });
 
 app.use(router);
+app.use(apolloProvider);
 app.mount('#app');
